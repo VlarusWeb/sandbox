cc7d73bacc9fa6b89f5c18e447596797037d353a
vlarusweb <vlarusweb@gmail.com>
2023-08-29 20:52:54 +0300

    feat: add solution calculate BMI

--- codewars/CalculateBMI.js
+++ codewars/CalculateBMI.js
@@ -12,3 +12,20 @@
 
 //.✅ Solution
 
+function bmi(weight, height) {
+  const bmi = weight / (height ** 2);
+  let resultMsg = "";
+  if(bmi <= 18.5) {
+    resultMsg = "Underweight";
+  } else if (bmi <= 25.0) {
+    resultMsg = "Normal";
+  } else if (bmi <= 30.0) {
+    resultMsg = "Overweight";
+  } else {
+    resultMsg = "Obese";
+  }
+  return resultMsg;
+}
+
+console.log(bmi(80, 1.80));
+console.log(bmi(102, 1.70));
