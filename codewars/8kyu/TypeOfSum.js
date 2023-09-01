b6d2f653975192ea7090443433a585381ad50eb3
vlarusweb <vlarusweb@gmail.com>
2023-09-01 17:35:31 +0300

    feat: add new solution for 'type of sum' kata

--- codewars/8kyu/TypeOfSum.js
+++ codewars/8kyu/TypeOfSum.js
@@ -23,3 +23,19 @@ console.log(typeOfSum1(null, false));
 
 
 
+// 🆕✅ Solution
+
+const typeOfSum = (a, b) => typeof(a + b);
+
+console.log(typeOfSum(12, 1));
+console.log(typeOfSum('d', 1));
+console.log(typeOfSum(12, 'a'));
+console.log(typeOfSum('dd', ''));
+console.log(typeOfSum(true, 1));
+console.log(typeOfSum('s', false));
+console.log(typeOfSum(null, 1));
+console.log(typeOfSum('s', null));
+console.log(typeOfSum(null, undefined));
+console.log(typeOfSum(undefined, 're'));
+console.log(typeOfSum(undefined, true));
+console.log(typeOfSum(null, false));
