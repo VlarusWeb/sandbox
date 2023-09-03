1a814878e788f89e760c6fcdd381c520b8cde6f8
vlarusweb <vlarusweb@gmail.com>
2023-09-03 23:17:08 +0300

    feat: add solution 'binary addition' kata

--- codewars/7kyu/BinaryAddition.js
+++ codewars/7kyu/BinaryAddition.js
@@ -8,3 +8,15 @@
 
 // 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+// ✅ Solution
+
+function addBinary(a,b) {
+  return (a + b).toString(2);
+};
+
+console.log(addBinary(1, 2));
+console.log(addBinary(18, 8));
+console.log(addBinary(9, 12));
+console.log(addBinary(14, 21));
+console.log(addBinary(10, 22));
