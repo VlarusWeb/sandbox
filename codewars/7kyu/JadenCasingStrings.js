dcf127b704cadf55a9bab3cf5b28522dc342b35b
vlarusweb <vlarusweb@gmail.com>
2023-09-07 23:41:41 +0300

    feat: add 'jaden casing strings' kata

---
+++ codewars/7kyu/JadenCasingStrings.js
@@ -0,0 +1,9 @@
+// ❓ Description
+
+// Your task is to convert strings to how they would be written by Jaden Smith. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+// Example:
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
