e2dc19bd87b03b1d3b586da25a569166e769b525
vlarusweb <vlarusweb@gmail.com>
2023-09-07 22:47:55 +0300

    feat: add solution for 'lost without a map' kata

--- codewars/8kyu/LostWithoutAMap.js
+++ codewars/8kyu/LostWithoutAMap.js
@@ -6,3 +6,19 @@
 
 // [1, 2, 3] --> [2, 4, 6]
 
+// ✅ Solution
+
+function maps(x) {
+  const doubledX = [];
+  x.forEach(el => {
+    let newEl = (el * 2);
+    doubledX.push(newEl);
+  });
+  return doubledX;
+}
+
+console.log(maps([1, 2, 3]));
+console.log(maps([5, 10, 20]));
+console.log(maps([2, 2, 2]));
+console.log(maps([1, 10, 100]));
+console.log(maps([11, 22, 33]));
