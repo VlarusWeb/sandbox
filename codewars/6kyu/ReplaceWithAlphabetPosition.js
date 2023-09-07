f7c1a8f9d57b0ac519ec92c8b06ff75e088d66e2
vlarusweb <vlarusweb@gmail.com>
2023-09-08 01:53:05 +0300

    feat: add solution for 'replace with alphabet position' kata

--- codewars/6kyu/ReplaceWithAlphabetPosition.js
+++ codewars/6kyu/ReplaceWithAlphabetPosition.js
@@ -12,3 +12,20 @@
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
+// ✅ Solution
+
+function alphabetPosition(text) {
+  const position = text
+      .toLowerCase()
+      .split("")
+      .map(char => {
+        let charCode = char.charCodeAt(0);
+        if (char >= 'a' && char <= 'z') {
+          return charCode - 'a'.charCodeAt(0) + 1;
+        }
+        return 0;
+      });
+      return position.filter(num => num !== 0).join(" ");
+}
+
+console.log(alphabetPosition("The sunset set at twelve o' clock."));
