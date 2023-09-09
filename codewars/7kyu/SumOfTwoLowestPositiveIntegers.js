944d1684ecedd48635f19dfbc6c66b2f343cc38b
vlarusweb <vlarusweb@gmail.com>
2023-09-10 02:13:25 +0300

    fead: add solution for 'sum of two lowest positive integer' kata

--- codewars/7kyu/SumOfTwoLowestPositiveIntegers.js
+++ codewars/7kyu/SumOfTwoLowestPositiveIntegers.js
@@ -6,3 +6,15 @@
 
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
 
+// ✅ Solution
+
+function sumTwoSmallestNumbers(numbers) {
+  return numbers.sort((a,b) => a - b).slice(0, 2).reduce((acc, num) => acc += num, 0);
+}
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22])); // 13
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43])); // 6
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7])); // 10
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1])); // 24
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4])); // 16
+console.log(sumTwoSmallestNumbers([22, 9, 81])); // 31
