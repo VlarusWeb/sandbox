d5733fc02de7d5c860b7b30589aded58ea47194d
vlarusweb <vlarusweb@gmail.com>
2023-09-09 23:17:01 +0300

    feat: add new solution for 'get the middle character' kata

--- codewars/7kyu/GetTheMiddleCharacter.js
+++ codewars/7kyu/GetTheMiddleCharacter.js
@@ -38,3 +38,15 @@ console.log(getMiddle("testing"));
 console.log(getMiddle("middle"));
 console.log(getMiddle("A"));
 console.log(getMiddle("Abracadabra"));
+
+
+
+// 🆕✅ New Solution
+
+const getMiddleChars = (s) => s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+
+console.log(getMiddleChars("test"));
+console.log(getMiddleChars("testing"));
+console.log(getMiddleChars("middle"));
+console.log(getMiddleChars("A"));
+console.log(getMiddleChars("Abracadabra"));
