19fc17c29c50ccacd4f183963d7813024ebbae15
vlarusweb <vlarusweb@gmail.com>
2023-09-10 14:59:01 +0300

    feat: add solution for 'exes and ohs' kata

--- codewars/7kyu/ExesAndOhs.js
+++ codewars/7kyu/ExesAndOhs.js
@@ -10,3 +10,25 @@
 // XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 // XO("zzoo") => false
 
+// ✅ Solution
+
+function XO(str) {
+  str = str.toLowerCase();
+  const obj = {};
+  for(let char of str) {
+    if(char === "x" || char === "o") {
+      if(obj[char] = obj[char]) {
+        obj[char] += 1;
+      } else {
+        obj[char] = 1;
+      }
+    }
+  } return obj.x === obj.o;
+}
+
+console.log(`return "${XO("ooxx")}", should be "true"`);
+console.log(`return "${XO("xooxx")}", should be "false"`);
+console.log(`return "${XO("ooxXm")}", should be "true"`);
+console.log(`return "${XO("abracadabra")}", should be "true"`); // becouse  both x and o are not present and both return undefined!
+console.log(`return "${XO("oozzyy")}", should be "false"`);
+console.log(`return "${XO("XoroxAndOxorx")}", should be "true"`);
