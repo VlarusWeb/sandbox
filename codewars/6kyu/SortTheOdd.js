6d2162d38ad4f7d206d3b39d0b16535ae2034d80
vlarusweb <vlarusweb@gmail.com>
2023-09-12 02:59:14 +0300

    feat: add solution for 'sort the odd' kata

--- codewars/6kyu/SortTheOdd.js
+++ codewars/6kyu/SortTheOdd.js
@@ -8,3 +8,29 @@
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
+// ✅ Solution
+
+function sortArray(array) {
+  let Odd = [];
+  for(let num of array) {
+    if(num % 2 !== 0) {
+      Odd.push(num);
+    }
+  }
+  const sortedOdd = Odd.sort((a, b) => a - b);
+  const result = [];
+  let i = 0;
+  for(let num of array) {
+    if(num % 2 !== 0) {
+      result.push(num = sortedOdd[i]);
+      i++;
+    } else {
+      result.push(num);
+    }
+  }
+  return result;
+}
+
+console.log(sortArray([5, 3, 2, 8, 1, 4]));
+console.log(sortArray([5, 3, 1, 8, 0]));
+console.log(sortArray([]));
