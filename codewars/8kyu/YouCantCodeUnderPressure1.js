aa80fe680323a996516dbd2eb6cf3ad5f7a227a2
vlarusweb <vlarusweb@gmail.com>
2023-09-15 23:07:49 +0300

    feat: add 'you can't code under pressure' kata

---
+++ codewars/8kyu/YouCantCodeUnderPressure1.js
@@ -0,0 +1,12 @@
+// ❓ Description
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// ✅ Solution
+
+const doubleInteger = (i) => i * 2;
+
+console.log(doubleInteger(2));
+console.log(doubleInteger(4));
+console.log(doubleInteger(5));
+console.log(doubleInteger(11));
