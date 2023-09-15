274efbb3516c1ba7d17cc626c358e0062e73446b
vlarusweb <vlarusweb@gmail.com>
2023-09-16 00:17:27 +0300

    feat: add solution for 'vowel count' kata

--- codewars/7kyu/VowelCount.js
+++ codewars/7kyu/VowelCount.js
@@ -6,3 +6,18 @@
 
 // The input string will only consist of lower case letters and/or spaces.
 
+// ✅ Solution
+
+function getCount(str) {
+let vowelsCount = 0;
+const vowels = ["a", "e", "o", "i", "u"];
+for(char of str) {
+  if(vowels.includes(char)) {
+  	vowelsCount++;
+    }
+	}
+	return vowelsCount;
+}
+
+console.log(getCount("abracadabra"));
+console.log(getCount("I'm the good programmer!"));
