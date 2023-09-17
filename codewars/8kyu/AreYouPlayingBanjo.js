ca0d12695cd82cf7b4e49944ab33d954c9b09518
vlarusweb <vlarusweb@gmail.com>
2023-09-17 23:52:53 +0300

    feat: add solution 'are you playing banjo' kata

--- codewars/8kyu/AreYouPlayingBanjo.js
+++ codewars/8kyu/AreYouPlayingBanjo.js
@@ -9,3 +9,20 @@
 // name + " does not play banjo"
 // Names given are always valid strings.
 
+// ✅ Solution
+
+function areYouPlayingBanjo(name) {
+  let msg = "";
+  if(name[0].match(/r/i)) {
+    msg = `${name} plays banjo`;
+  } else {
+    msg = `${name} does not play banjo`;
+  }
+  return msg;
+}
+
+console.log(areYouPlayingBanjo("Adam")); //"Adam does not play banjo");
+console.log(areYouPlayingBanjo("Paul")); //"Paul does not play banjo");
+console.log(areYouPlayingBanjo("Ringo")); //"Ringo plays banjo");
+console.log(areYouPlayingBanjo("bravo")); //"bravo does not play banjo");
+console.log(areYouPlayingBanjo("rolf")); //"rols plays banjo");
