d4980cc06667be31863656dcc6aa0e68ac6c0004
vlarusweb <vlarusweb@gmail.com>
2023-09-22 10:11:44 +0300

    feat: add solution for 'calculate average' kata

--- codewars/8kyu/CalculateAverage.js
+++ codewars/8kyu/CalculateAverage.js
@@ -4,3 +4,12 @@
 
 // Note: Empty arrays should return 0.
 
+// ✅ Solution
+
+function findAverage(array) {
+  return array.length === 0 ? 0 : array.reduce((acc, num) => acc += num) / array.length;
+}
+
+console.log(findAverage([1, 1, 1])); // should return 1;
+console.log(findAverage([1, 2, 3])); // should return 2;
+console.log(findAverage([1, 2, 3, 4])); // should return 2.5;
