131214695e1a3acecce3137e091b5f74e0d52edd
vlarusweb <vlarusweb@gmail.com>
2023-09-23 23:27:26 +0300

    feat: add solution to 'keep up the hoop' kata

--- codewars/8kyu/KeepUpTheHoop.js
+++ codewars/8kyu/KeepUpTheHoop.js
@@ -7,3 +7,11 @@
 // If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
+// ❕✅ Solution
+
+function hoopCount(n) {
+  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+}
+
+console.log(hoopCount(3));
+console.log(hoopCount(11));
