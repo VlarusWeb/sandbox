f10e4e14a439e814c560995cc29d1c060dcbc914
vlarusweb <vlarusweb@gmail.com>
2023-09-25 02:40:03 +0300

    feat: add solution for 'is he gonna survive' kata

--- codewars/8kyu/IsHeGonnaSurvive.js
+++ codewars/8kyu/IsHeGonnaSurvive.js
@@ -4,3 +4,14 @@
 
 // Return true if yes, false otherwise :)
 
+// ✅ Solution
+
+const hero = (bullets, dragons) => dragons * 2 <= bullets ? true : false;
+
+console.log(hero(10, 5));
+console.log(hero(7, 4));
+console.log(hero(4, 5));
+console.log(hero(100, 40));
+console.log(hero(1500, 751));
+console.log(hero(0, 1));
+console.log(hero(18425, 9213));
