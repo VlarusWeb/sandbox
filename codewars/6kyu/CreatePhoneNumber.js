72927c6e4a734a39bd4944590cfc30024029367f
vlarusweb <vlarusweb@gmail.com>
2023-09-25 17:11:53 +0300

    feat: add new solution for 'create phone number' kata

--- codewars/6kyu/CreatePhoneNumber.js
+++ codewars/6kyu/CreatePhoneNumber.js
@@ -16,3 +16,11 @@ const createPhoneNumber = (numbers) => ["(", numbers.slice(0, 3).join(""), ") ",
 const testPhoneNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 console.log(createPhoneNumber(testPhoneNumber));
+
+
+
+// ❕✅ New Solution
+
+const formatPhoneNumber = (numbers) => numbers.reduce((form, dig) => form.replace("x", dig), "(xxx) xxx-xxxx");
+
+console.log(formatPhoneNumber(testPhoneNumber));
