1891628f18ed2cd55347f0680fe2476214ac95b0
vlarusweb <vlarusweb@gmail.com>
2023-09-25 16:59:41 +0300

    feat: add solution for 'create phone number' kata

--- codewars/6kyu/CreatePhoneNumber.js
+++ codewars/6kyu/CreatePhoneNumber.js
@@ -8,3 +8,11 @@
 
 // Don't forget the space after the closing parentheses!
 
+// ✅ Solution
+
+const createPhoneNumber = (numbers) => ["(", numbers.slice(0, 3).join(""), ") ", numbers.slice(3, 6).join(""), "-", numbers.slice(6, 10).join("")].join("");
+
+
+const testPhoneNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+console.log(createPhoneNumber(testPhoneNumber));
