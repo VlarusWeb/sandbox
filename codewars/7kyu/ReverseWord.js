b32184e608f67bb523556dfe3a4afc86fede731e
vlarusweb <vlarusweb@gmail.com>
2023-09-29 15:56:33 +0300

    fix: add solution comment for 'reverse word' kata

--- codewars/7kyu/ReverseWord.js
+++ codewars/7kyu/ReverseWord.js
@@ -6,6 +6,8 @@
 // "This is an example!" ==> "sihT si na !elpmaxe"
 // "double  spaces"      ==> "elbuod  secaps"
 
+// ✅ Solution
+
 const reverseWord = (str) => str.split(" ").map(el => el.split("").reverse().join("")).join(" ");
 
 console.log(reverseWord("The quick brown fox jumps over the lazy dog."));
