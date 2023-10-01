0689647eb38f839f6b7a6a0de7a3720098e6ee10
vlarusweb <vlarusweb@gmail.com>
2023-10-01 21:00:24 +0300

    feat: add sulition for 'twice as old' kata

--- codewars/8kyu/TwiceAsOld.js
+++ codewars/8kyu/TwiceAsOld.js
@@ -6,3 +6,12 @@
 // current age of his son (years)
 // Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
 
+// ✅  Solution
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - (sonYearsOld * 2));
+
+console.log(twiceAsOld(36, 7));
+console.log(twiceAsOld(55, 30));
+console.log(twiceAsOld(42, 21));
+console.log(twiceAsOld(22, 1));
+console.log(twiceAsOld(29, 0));
