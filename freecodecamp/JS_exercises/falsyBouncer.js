daa4cc532d44fc5a0442119666679bbf9c8d71a9
vlarusweb <vlarusweb@gmail.com>
2023-10-08 17:40:59 +0300

    feat: add solution for 'falsy bounce' exercise

--- freecodecamp/JS_exercises/falsyBouncer.js
+++ freecodecamp/JS_exercises/falsyBouncer.js
@@ -5,3 +5,12 @@
 
 // Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 
+// ✅ Solution
+
+const bouncer = (arr) => [...arr.filter((el) => el ? el : null)];
+
+console.log(bouncer([7, "ate", "", false, 9]));
+console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer(["a", "b", "c"]));
+console.log(bouncer([null, NaN, 1, 2, undefined]));
+console.log(bouncer([7, " ", "", false, "10"]));
