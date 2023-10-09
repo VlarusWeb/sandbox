725baf224122d289dc28724a9db62abeba2e0d21
vlarusweb <vlarusweb@gmail.com>
2023-10-09 21:51:51 +0300

    feat: add solution 'where do I belong' exercise

--- freecodecamp/JS_exercises/whereDoIBelong.js
+++ freecodecamp/JS_exercises/whereDoIBelong.js
@@ -7,3 +7,10 @@
 
 // Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
 
+// ✅ Solution
+
+const getIndexToIns = (arr, num) => [...arr, num].sort((a, b) => a - b).indexOf(num);
+
+console.log(getIndexToIns([1, 2, 3, 4, 5], 1.5));
+console.log(getIndexToIns([20, 3, 5], 19));
+console.log(getIndexToIns([20, 30, 40, 50], 35));
