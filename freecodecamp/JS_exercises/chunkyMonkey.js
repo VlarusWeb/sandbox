7305f3a73c67e1ab411f9a8259e11c94497c0263
vlarusweb <vlarusweb@gmail.com>
2023-10-11 23:42:33 +0300

    feat: add solution for 'chunky monkey' exercise

--- freecodecamp/JS_exercises/chunkyMonkey.js
+++ freecodecamp/JS_exercises/chunkyMonkey.js
@@ -2,3 +2,15 @@
 
 // Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
+const chunkArrayInGroups = (arr, size) => {
+    const chunkedArray = [];
+    for(let i = 0; i < arr.length; i += size) {;
+        chunkedArray.push(arr.slice(i, i + size));
+    }
+    return chunkedArray;
+}
+
+console.log(chunkArrayInGroups([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
+console.log(chunkArrayInGroups([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(chunkArrayInGroups(["a", "b", "c", "d", "e", "f", "g"], 4));
+console.log(chunkArrayInGroups(["I", "am", "a", "very", "good", "programmer", "!"], 2));
