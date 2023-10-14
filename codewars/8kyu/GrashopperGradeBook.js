077e1f08514643ca7284333ceaad5cf83371d1f5
vlarusweb <vlarusweb@gmail.com>
2023-10-15 02:29:28 +0300

    feat: add solution for 'Grasshopper Grade Book' kata

--- codewars/8kyu/GrashopperGradeBook.js
+++ codewars/8kyu/GrashopperGradeBook.js
@@ -10,3 +10,25 @@
 // 0 <= score < 60	'F'
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 
+// ✅ Solution
+
+const getGrade = (s1, s2, s3) => {
+  let average = (s1 + s2 + s3) / 3;
+  if (average >= 90) {
+    return "A";
+  } else if (average >= 80) {
+    return "B";
+  } else if (average >= 70) {
+    return "C";
+  } else if (average >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+console.log(getGrade(95, 93, 90));
+console.log(getGrade(78, 93, 70));
+console.log(getGrade(65, 83, 70));
+console.log(getGrade(65, 73, 69));
+console.log(getGrade(45, 43, 40));
