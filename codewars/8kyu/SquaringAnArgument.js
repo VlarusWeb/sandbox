b502ca071c87f28a12b5f0e23bc07238be6decb7
vlarusweb <vlarusweb@gmail.com>
2023-10-14 22:49:58 +0300

    feat: add solution for square an argument' kata

--- codewars/8kyu/SquaringAnArgument.js
+++ codewars/8kyu/SquaringAnArgument.js
@@ -2,3 +2,11 @@
 
 // Now you have to write a function that takes an argument and returns the square of it.
 
+// ✅ Solution
+
+const square = (num) => num * num;
+
+console.log(square(5));
+console.log(square(7));
+console.log(square(3));
+console.log(square(9));
