799427f4f99a39653199a5a16c33fcdf829c1e0c
vlarusweb <vlarusweb@gmail.com>
2023-10-19 13:10:16 +0300

    feat: add solution for 'find the odd int' kata

--- codewars/6kyu/FindTheOddInt.js
+++ codewars/6kyu/FindTheOddInt.js
@@ -11,3 +11,26 @@
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
+// ✅ Solution
+
+function findOdd(arr) {
+  let obj = {};
+  arr.forEach(element => {
+    if (obj[element] === undefined) {
+      obj[element] = 1;
+    } else {
+      obj[element]++;
+    }
+  });
+  for (key in obj) {
+    if (obj[key] % 2 !== 0) {
+      return +key;
+    }
+  }
+}
+
+console.log(findOdd([7]));
+console.log(findOdd([0]));
+console.log(findOdd([1, 1, 2]));
+console.log(findOdd([0, 1, 0, 1, 0]));
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
