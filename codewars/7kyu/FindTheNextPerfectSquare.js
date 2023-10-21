f363d7be78691a3e22b983442c76cdfb0c5e1922
vlarusweb <vlarusweb@gmail.com>
2023-10-21 23:10:11 +0300

    feat: add solution for 'find the next perfect square' kata

--- codewars/7kyu/FindTheNextPerfectSquare.js
+++ codewars/7kyu/FindTheNextPerfectSquare.js
@@ -12,3 +12,12 @@
 // 625 --> 676
 // 114 --> -1 since 114 is not a perfect square
 
+// ✅ Solution
+
+const findNextSquare = (sq) => Number.isInteger(Math.sqrt(sq)) ? Math.pow(Math.sqrt(sq) + 1, 2) : -1;
+
+console.log(findNextSquare(121));
+console.log(findNextSquare(625));
+console.log(findNextSquare(319225));
+console.log(findNextSquare(15241383936));
+console.log(findNextSquare(155));
