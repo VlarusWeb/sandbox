8c73cf6183d30b5158bd2dba048762061dd4ffff
vlarusweb <vlarusweb@gmail.com>
2023-10-22 14:55:20 +0300

    feat: add solution for 'array diff' kata

--- codewars/6kyu/ArrayDiff.js
+++ codewars/6kyu/ArrayDiff.js
@@ -9,3 +9,12 @@
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
+// ✅ Solution
+
+const arrayDiff = (a, b) => a.filter((el) => !b.includes(el));
+
+console.log(arrayDiff([1, 2], [1]));
+console.log(arrayDiff([1, 2, 3], [1, 2]));
+console.log(arrayDiff([1, 2, 2], [2]));
+console.log(arrayDiff([1, 2, 2], []));
+console.log(arrayDiff([], [1, 2]));
