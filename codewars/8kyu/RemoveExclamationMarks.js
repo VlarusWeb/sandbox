d0430768054e704e995e011d6fbe141293a1111a
vlarusweb <vlarusweb@gmail.com>
2023-10-27 08:11:10 +0300

    feat: add solution for 'remove exclamation mark' kata

--- codewars/8kyu/RemoveExclamationMarks.js
+++ codewars/8kyu/RemoveExclamationMarks.js
@@ -2,3 +2,10 @@
 
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
+// ✅ Solution
+
+const removeExclamationMarks = (s) => s.replace(/!/g, "");
+
+console.log(removeExclamationMarks("Hello World!"));
+console.log(removeExclamationMarks("I'm a really good programmer!"));
+console.log(removeExclamationMarks("Hello World! I'm the programmer!"));
