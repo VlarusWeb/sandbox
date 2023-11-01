ec1b436951dec49de226cd6629609bd9a74733c9
vlarusweb <vlarusweb@gmail.com>
2023-11-01 20:14:38 +0200

    feat: add solution for 'sort and star' kata

--- codewars/8kyu/SortAndStar.js
+++ codewars/8kyu/SortAndStar.js
@@ -6,3 +6,9 @@
 
 // You should not remove or add elements from/to the array.
 
+// ✅ Solution
+
+const twoSort = (s) => [...s.sort()][0].split("").join("***");
+
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]));
+console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]));
