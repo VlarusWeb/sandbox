c4d8878b8fc7244fc3fa16fbb367591a91793fa0
vlarusweb <vlarusweb@gmail.com>
2023-11-01 20:23:40 +0200

    fix: corrected solution for 'sort and star' kata

--- codewars/8kyu/SortAndStar.js
+++ codewars/8kyu/SortAndStar.js
@@ -8,7 +8,7 @@
 
 // ✅ Solution
 
-const twoSort = (s) => [...s.sort()][0].split("").join("***");
+const twoSort = (s) => s.sort()[0].split("").join("***");
 
 console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]));
 console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]));
