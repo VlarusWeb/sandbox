14f9df6fd616609b3b5d2f4bc52059b6097c092f
vlarusweb <vlarusweb@gmail.com>
2023-11-02 16:10:02 +0200

    feat: add solution for 'remove anchor from url' kata

--- codewars/7kyu/RemoveAnchorFromURL.js
+++ codewars/7kyu/RemoveAnchorFromURL.js
@@ -6,3 +6,12 @@
 // "www.codewars.com#about" --> "www.codewars.com"
 // "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
+
+
+// ✅ Solution
+
+const removeUrlAnchor = (url) => url.includes("#") ? url.slice(0, url.indexOf("#")) : url;
+
+console.log(removeUrlAnchor("www.codewars.com#about"));
+console.log(removeUrlAnchor("www.codewars.com/katas/?page=1#about"));
+console.log(removeUrlAnchor("www.codewars.com/katas"));
