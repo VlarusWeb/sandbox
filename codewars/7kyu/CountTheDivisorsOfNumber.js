0a50b17a06d8995ca11afe50997f8bc4595c38eb
vlarusweb <vlarusweb@gmail.com>
2023-11-12 18:38:10 +0200

    feat: add solution for 'count the devisors of a number' kata

--- codewars/7kyu/CountTheDivisorsOfNumber.js
+++ codewars/7kyu/CountTheDivisorsOfNumber.js
@@ -9,3 +9,21 @@
 // 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
 // Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
 
+// ✅ Solution
+
+function getDivisionCnt(n) {
+  let divisors = 0;
+  for (let i = 1; i * i <= n; i++) {
+    if (n % i === 0) {
+      divisors++;
+      if (i !== n / i) {
+      divisors++;
+      }
+    }
+  } return divisors;
+}
+
+console.log(getDivisionCnt(1));  //Should return 1
+console.log(getDivisionCnt(10)); //Should return 4
+console.log(getDivisionCnt(11)); //Should return 2
+console.log(getDivisionCnt(54)); //Should return 8
