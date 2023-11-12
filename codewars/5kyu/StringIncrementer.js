e57baa14d9b0d78b41aacb9c92954d9d933b324c
vlarusweb <vlarusweb@gmail.com>
2023-11-13 00:17:39 +0200

    feat: add solution for 'string incrementer' kata

--- codewars/5kyu/StringIncrementer.js
+++ codewars/5kyu/StringIncrementer.js
@@ -17,3 +17,30 @@
 // foo099 -> foo100
 
 // Attention: If the number has leading zeros the amount of digits should be considered.
+
+
+
+// ✅ Solution
+
+function incrementString(string) {
+  if (!string.match(/[0-9]/)) {
+    return string + 1;
+  }
+  const splitedNumPart = parseInt(string.split("").reverse().join("")).toString().split("").reverse().join("");
+  const incrementedNum = (+splitedNumPart + 1).toString();
+  const zeros = [];
+  if (splitedNumPart.includes("0")) {
+    for (let i = 0; i < splitedNumPart.length - incrementedNum.length; i++) {
+      zeros.push("0");
+    }
+  } return string.slice(0, -splitedNumPart.length) + zeros.join("") + incrementedNum;
+}
+
+console.log(incrementString("foobar000"));
+console.log(incrementString("foobar999"));
+console.log(incrementString("foobar00999"));
+console.log(incrementString("foobar"));
+console.log(incrementString("foobar1"));
+console.log(incrementString("1"));
+console.log(incrementString("009"));
+console.log(incrementString("foo99bar99"));
