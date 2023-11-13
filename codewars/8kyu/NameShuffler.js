0791687df1e9e55181cb6fd140dddb3b065bbce0
vlarusweb <vlarusweb@gmail.com>
2023-11-13 23:40:19 +0200

    feat: add 'name shuffler' kata

---
+++ codewars/8kyu/NameShuffler.js
@@ -0,0 +1,9 @@
+// ❔ Description
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+
