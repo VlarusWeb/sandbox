4f55394bde390012c47d113adcc8b6dc354510e0
vlarusweb <vlarusweb@gmail.com>
2023-11-17 03:04:02 +0200

    feat: add solution to 'get character from ASCII values' kata

--- codewars/8kyu/GetCharacterFromASCllValue.js
+++ codewars/8kyu/GetCharacterFromASCllValue.js
@@ -9,3 +9,20 @@
 // 48 --> '0
 // For ASCII table, you can refer to http://www.asciitable.com/
 
+
+
+// ✅ Solution
+
+const getChar = (c) => String.fromCharCode(c);
+
+console.log(getChar(55));
+console.log(getChar(56));
+console.log(getChar(57));
+console.log(getChar(58));
+console.log(getChar(59));
+console.log(getChar(60));
+console.log(getChar(61));
+console.log(getChar(62));
+console.log(getChar(63));
+console.log(getChar(64));
+console.log(getChar(65));
