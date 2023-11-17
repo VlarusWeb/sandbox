aaaa64781ba971bb71f066bd3c6a7bbc984be6aa
vlarusweb <vlarusweb@gmail.com>
2023-11-17 03:49:08 +0200

    feat: add solution to 'you're a square' kata

--- codewars/8kyu/YouAreASquare.js
+++ codewars/8kyu/YouAreASquare.js
@@ -22,3 +22,13 @@
 
 
 
+// ✅ Solution
+
+const isSquare = (n) => Number.isInteger(Math.sqrt(n)) ;
+
+console.log(isSquare(-1));
+console.log(isSquare(0));
+console.log(isSquare(3));
+console.log(isSquare(4));
+console.log(isSquare(25));
+console.log(isSquare(26));
