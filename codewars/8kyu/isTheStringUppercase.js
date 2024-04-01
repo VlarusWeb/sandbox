70f3a3fb55ce89e98ab390f64dfe832ad4b3e939
vlarusweb <vlarusweb@gmail.com>
2024-04-01 19:56:57 +0300

    feat: add 'is the string uppercase' kata

---
+++ codewars/8kyu/isTheStringUppercase.js
@@ -0,0 +1,14 @@
+// Description!
+
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+// ✅ Solution
+
