a10b8db184e45b507924ba8c42336318f88bc936
vlarusweb <vlarusweb@gmail.com>
2024-04-03 00:15:05 +0300

    feat: add solution to 'hex to decimal' task

--- codewars/8kyu/HexToDecimal.js
+++ codewars/8kyu/HexToDecimal.js
@@ -1,6 +1,15 @@
-// Complete the function which convert hex number (given as a string) to a decimal number.
+// ❔Complete the function which convert hex number (given as a string) to a decimal number.
+
+// ✅ Solution
 
 function hexToDex(hexString) {
   return parseInt(hexString, 16);
 }
 
+// ❕❔ Tests
+
+console.log(hexToDex("1"));
+console.log(hexToDex("a"));
+console.log(hexToDex("ff"));
+console.log(hexToDex("10"));
+console.log(hexToDex("-A"));
