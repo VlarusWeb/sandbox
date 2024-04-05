81a9103165faf2fc1c230ca34f34de0fc7b08b24
vlarusweb <vlarusweb@gmail.com>
2024-04-05 21:47:02 +0300

    feat: add solution to 'Add length' task

--- codewars/8kyu/AddLength.js
+++ codewars/8kyu/AddLength.js
@@ -10,3 +10,20 @@
 
 // Note: String will have at least one element; words will always be separated by a space.
 
+
+
+// ✅ Solution
+
+function addLength(str) {
+  let result = [];
+  str.split(" ").forEach(word => result.push(`${word} ${word.length}`));
+  return result;
+}
+
+// ❕❔ Tests
+
+console.log(addLength("I am a good programmer!"));
+console.log(addLength("Hello word!"));
+console.log(addLength("I'm all the way up!"));
+console.log(addLength("Nothing can stop me!"));
+console.log(addLength("Today is that day!"));
