361f9a214e78fa614b965d762f29dc2998377f7a
vlarusweb <vlarusweb@gmail.com>
2024-04-06 22:13:54 +0300

    feat: add solution to 'Welcome to the City' task

--- codewars/8kyu/WelcomeToTheCity.js
+++ codewars/8kyu/WelcomeToTheCity.js
@@ -7,3 +7,18 @@
 // ['John', 'Smith'], 'Phoenix', 'Arizona'
 // This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
 
+
+
+// ✅ Solution
+
+function sayHello(name, city, state) {
+  return "Hello, " + name.join(" ") + "! " + "Welcome to " + city + ", " + state + "!";
+}
+
+// ❕❔ Tests
+
+console.log(sayHello(["John", "Smith"], "Phenix", "Arizona"));
+console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'));
+console.log(sayHello(['Wallace','Russel','Osbourne'],'Albany','New York'));
+console.log(sayHello(["John", "Week"], "New York City", "New York State"));
+console.log(sayHello(["John", "Snow"], "the Tower of joy", "Red Mountains of Dorne"));
